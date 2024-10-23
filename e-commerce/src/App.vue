417358da6a99ff8d7029af93b382582e5c79e16a
lymakara-dev <ly.makara.personal@gmail.com>
2024-10-23 10:04:49 +0700

    TP01: Finish step 3
        *Set up a vuejs project
        *Clean unused committed
        *Message "This is my first vuejs project"

---
+++ e-commerce/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  This is my first Vue.js project.
+
+  <RouterView />
+</template>
+
+<style scoped></style>
